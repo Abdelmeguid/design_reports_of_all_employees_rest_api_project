85d50733f826815db616f09d4b2abd0f7a5cf5e1
BEDO <siegen567@gmail.com>
2023-06-18 03:11:20 +0300

    rr
    rr

--- server/index.js
+++ server/index.js
@@ -50,8 +50,7 @@ const fileFilter = (req, file, cb) => {
 };
 
 app.get("/", (req, res) => {
-  // res.send("Hello, Express!");
-  res.redirect("/feed");
+  res.send("Hello, Express!");
 });
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
@@ -81,12 +80,15 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    // useFindAndModify: false,
-    // useCreateIndex: true,
-    // useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://ahmed:qwerty666666@cluster0.goxgywv.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      // useFindAndModify: false,
+      // useCreateIndex: true,
+      // useUnifiedTopology: true,
+    }
+  )
   .then((result) => {
     app.listen(8080);
   })
