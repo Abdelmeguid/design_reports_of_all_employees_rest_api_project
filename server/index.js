b2bcd51f739f8992e079b7da9941027978a14886
BEDO <siegen567@gmail.com>
2023-06-22 15:53:54 +0300

    update
    update

--- server/index.js
+++ server/index.js
@@ -3,13 +3,88 @@
 // dotenv.config();
 //const dotenv = require("dotenv");
 
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const multer = require("multer");
+// const cors = require("cors");
+// //const { check } = require("express-validator/check");
+// //const { body, validationResult } = require("express-validator");
+
+// const feedRoutes = require("./routes/feed");
+// const authRoutes = require("./routes/auth");
+
+// const app = express();
+
+// // const fileStorage = multer.diskStorage({
+// //   destination: (req, file, cb) => {
+// //     cb(null, "images");
+// //   },
+// //   filename: (req, file, cb) => {
+// //     cb(null, new Date().toISOString() + "-" + file.originalname);
+// //   },
+// // });
+
+// const { v4: uuidv4 } = require("uuid");
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "images");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, uuidv4());
+//   },
+// });
+
+// const fileFilter = (req, file, cb) => {
+//   if (
+//     file.mimetype === "image/png" ||
+//     file.mimetype === "image/jpg" ||
+//     file.mimetype === "image/jpeg" ||
+//     file.mimetype === "image/webp"
+//   ) {
+//     cb(null, true);
+//   } else {
+//     cb(null, false);
+//   }
+// };
+
+// // app.get("/", (req, res) => {
+// //   res.send("Hello, Express!");
+// // });
+
+// // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+// app.use(bodyParser.json()); // application/json
+// app.use(multer({ storage: storage, fileFilter: fileFilter }).single("image"));
+// app.use("/images", express.static(path.join(__dirname, "images")));
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
+
+// app.use("/feed", feedRoutes);
+// app.use("/auth", authRoutes);
+
+// app.use((error, req, res, next) => {
+//   console.log(error);
+//   const status = error.statusCode || 500;
+//   const message = error.message;
+//   const data = error.data;
+//   res.status(status).json({ message: message, data: data });
+// });
+
 const path = require("path");
 require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const multer = require("multer");
-const cors = require("cors");
 //const { check } = require("express-validator/check");
 //const { body, validationResult } = require("express-validator");
 
@@ -51,9 +126,9 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-// app.get("/", (req, res) => {
-//   res.send("Hello, Express!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
@@ -87,22 +162,27 @@ mongoose
   })
 
   .then((result) => {
-    app.use(cors());
-    app.use(express.static(path.join(__dirname, "build")));
-
-    app.get("/", (req, res) => {
-      res.sendFile(path.join(__dirname, "build", "index.html"));
-    });
-
-    app.listen(8080, () => {
-      console.log("Server is listening on port 8080");
-    });
+    app.listen(8080);
   })
   .catch((err) => console.log(err));
 
-//i add below for deployment
+//   .then((result) => {
+//     // app.use(cors());
+//     // app.use(express.static(path.join(__dirname, "build")));
 
-//i comment below line as per stalkoverflow
+//     // app.get("/", (req, res) => {
+//     //   res.sendFile(path.join(__dirname, "build", "index.html"));
+//     // });
+
+//     app.listen(8080, () => {
+//       console.log("Server is listening on port 8080");
+//     });
+//   })
+//   .catch((err) => console.log(err));
+
+// //i add below for deployment
+
+// //i comment below line as per stalkoverflow
 
 // const __dirname2 = path.resolve();
 // app.use("/uploads", express.static(path.join(__dirname2, "/uploads")));
